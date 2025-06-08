81be0f8cbaa5714b8c823ebc22fe1509b2186946
Mari <mariscoralich@gmail.com>
2025-06-08 17:49:07 -0300

    correção da caixa de aviso

--- js/script.js
+++ js/script.js
@@ -1,4 +1,4 @@
-document.getElementById("botaoEnviar").addEventListener("click", validaFormulario)
+document.getElementById("botao-enviar").addEventListener("click", validaFormulario)
 
 function validaFormulario(){
   if(document.getElementById("nome").value != ""
